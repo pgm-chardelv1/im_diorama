b5554d8505ec77e9b6fb8f92ee54d1c97f288cd5
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2021-11-11 15:05:58 +0100

    Clean up glb file

--- src/App/components/House/Bats.jsx
+++ src/App/components/House/Bats.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
-import { useGLTF, useMatcapTexture } from "@react-three/drei";
-import { DoubleSide } from "three";
+import { useGLTF } from "@react-three/drei";
 import { useFrame } from '@react-three/fiber';
 
-import MatCap from "../../lib/MatCap";
 import batGlb from './assets/house.glb';
 import { AnimationMixer } from 'three';
 
@@ -11,9 +9,6 @@ const Bat = () => {
     const { animations, scene } = useGLTF(batGlb, true);
     const animationRef = useRef();
     const mixer = useMemo(() => new AnimationMixer(), []);
-    const [matcap] = useMatcapTexture(MatCap.id.BLACK, MatCap.size.XS);
-    const [toggle, setToggle] = useState(true);
-    const scale = toggle ? 1 : 2;
 
     useEffect(() => {
         if (animationRef.current) {
@@ -24,17 +19,6 @@ const Bat = () => {
     useFrame((state, delta) => {
         mixer.update(delta);
     });
-    {/* <group>
-    <mesh geometry={nodes.Bat.geometry} scale={1} position={[-4,-1, 3]}>
-        <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
-    </mesh>
-    <mesh geometry={nodes.Bat.geometry} scale={1} position={[-3, 3, 7]} rotation={[0, 45, 0]}>
-        <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
-    </mesh>
-    <mesh onClick={() =>  setToggle(!toggle)} geometry={nodes.Bat.geometry} scale={scale} position={[-4,-1, 2]} rotation={[0, -45, 0]}>
-        <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
-    </mesh>
-</group> */}
 
     return (
         <primitive object={scene} ref={animationRef} />)
