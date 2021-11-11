8bf06204a900471fbe12006186352488d7a1835d
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2021-11-11 14:14:30 +0100

    Add different texture for clickable cat

--- src/App/components/House/Cats.jsx
+++ src/App/components/House/Cats.jsx
@@ -8,19 +8,20 @@ import catGlb from './assets/house.glb';
 const Cat = () => {
     const { nodes } = useGLTF(catGlb, true);
     const [matcap] = useMatcapTexture(MatCap.id.BLACK, MatCap.size.XS);
+    const [clickableMatCap] = useMatcapTexture(MatCap.id.GREEN, MatCap.size.XL);
     const [toggle, setToggle] = useState(true);
     const scale = toggle ? .01 : 0.1;
 
     return (
         <group>
-            <mesh geometry={nodes.Cat.geometry} scale={0.01} position={[-4,-1, 3]}>
+            <mesh geometry={nodes.Cat.geometry} scale={0.01} position={[-4,0, 3]}>
                 <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
             </mesh>
-            <mesh geometry={nodes.Cat.geometry} scale={0.01} position={[-3,-1, 3]} rotation={[0, 45, 0]}>
+            <mesh geometry={nodes.Cat.geometry} scale={0.01} position={[-3, 0, 3]} rotation={[0, 45, 0]}>
                 <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
             </mesh>
-            <mesh onClick={() =>  setToggle(!toggle)} geometry={nodes.Cat.geometry} scale={scale} position={[-4,-1, 2]} rotation={[0, -45, 0]}>
-                <meshMatcapMaterial matcap={matcap} side={DoubleSide}/>
+            <mesh onClick={() =>  setToggle(!toggle)} geometry={nodes.Cat.geometry} scale={scale} position={[-4, 0, 2]} rotation={[0, -45, 0]}>
+                <meshMatcapMaterial matcap={clickableMatCap} side={DoubleSide}/>
             </mesh>
         </group>
     );
--- src/App/lib/MatCap/index.js
+++ src/App/lib/MatCap/index.js
@@ -2,6 +2,7 @@
 const MatCap = {
   id: {
     GREEN: "54C104_BBFA0F_97EF04_7AE104",
+    BLACK: "1B1B1B_515151_7E7E7E_6C6C6C",
   },
   size: {
     XS: 64,
@@ -10,16 +11,6 @@ const MatCap = {
     LG: 512,
     XL: 1024,
   },
-  id: {
-    BLACK: '1B1B1B_515151_7E7E7E_6C6C6C',
-    size: {
-      XS: 64,
-      SM: 128,
-      MD: 256,
-      LG: 512,
-      XL: 1024,
-    }
-  }
 };
 
 export default MatCap;
